f1e2860cdb0ac0d8f43b81fce404df13fc064f3a
Dominik <dominik.gasperovic@vvk.lt>
2026-05-05 21:11:31 +0300

    commit

--- resources/views/admin/conferences/index.blade.php
+++ resources/views/admin/conferences/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="d-flex justify-content-between align-items-center">
     <h1>{{ __('messages.conference_management') }}</h1>
-    <a class="btn btn-primary" href="{{ route('admin.conferences.create') }}">{{ __('messages.create_conference') }}</a>
+    <a class="btn btn-success" href="{{ route('admin.conferences.create') }}">{{ __('messages.create_conference') }}</a>
 </div>
 <table class="table table-striped bg-white shadow-sm mt-3">
     <thead><tr><th>{{ __('messages.title') }}</th><th>{{ __('messages.date') }}</th><th>{{ __('messages.address') }}</th><th>{{ __('messages.actions') }}</th></tr></thead>
@@ -14,7 +14,7 @@
             <td>{{ $conference['date'] }} {{ $conference['time'] }}</td>
             <td>{{ $conference['address'] }}</td>
             <td>
-                <a class="btn btn-sm btn-outline-primary" href="{{ route('admin.conferences.show', $conference['id']) }}">{{ __('messages.view') }}</a>
+                <a class="btn btn-sm btn-outline-success" href="{{ route('admin.conferences.show', $conference['id']) }}">{{ __('messages.view') }}</a>
                 <a class="btn btn-sm btn-warning" href="{{ route('admin.conferences.edit', $conference['id']) }}">{{ __('messages.edit') }}</a>
                 <form action="{{ route('admin.conferences.destroy', $conference['id']) }}" method="POST" class="d-inline js-delete-form">
                     @csrf
